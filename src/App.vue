4ba0fd295eb39b5f1daacdb2e361d3de33b3ced6
Sergio Vasquez Omonte <yoniko10@gmail.com>
2021-02-28 22:05:13 -0300

    primer commit JallallaSD

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,26 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="body">
+    <router-view />
   </div>
-  <router-view />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+body{
+  margin: 0 0 0 0;
+  padding: 0 0 0 0;
+}
+
+#app{
+  font-family: Avenir, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #000000;
+  margin-top: 0px;
 }
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.title-component{
+    padding: 30px 0 10px 0;
 }
 </style>
